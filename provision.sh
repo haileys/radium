2e3bd5f53017523ea1937908163f94a26d54ee86
Charlie Somerville <charlie@charliesomerville.com>
2013-12-14 20:20:23 +1100

    get a decent shell prompt going

--- provision.sh
+++ provision.sh
@@ -8,3 +8,11 @@ sudo apt-get install -y build-essential nasm git mtools
 
 # development niceties
 sudo apt-get install -y vim gdb
+
+# get a decent shell prompt going
+cat > ~/.bash_profile <<SH
+function current-branch {
+    git branch --color=never 2>/dev/null | grep '^*' | cut -b2-
+}
+export PS1=$'\e[33;1m\w\e[36m\$(current-branch) \e[32mλ\e[0m '
+SH

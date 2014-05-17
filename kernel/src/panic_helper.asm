cd43bc70a9356df3ed311d41cb00de595a29daf5
Charlie Somerville <charlie@charliesomerville.com>
2014-05-17 17:48:07 +1000

    remove panic_backtrace_item definition from panic_helper.asm

--- kernel/src/panic_helper.asm
+++ kernel/src/panic_helper.asm
@@ -7,8 +7,6 @@ extern panic_print_backtrace_item
 
 section .rodata
 
-panic_backtrace_item db "  [<0x%x>]", 10, 0
-
 panic_symbols:
     db '@@@ PANIC SYMBOL TABLE @@@'
     times 65536 - ($ - panic_symbols) db 0

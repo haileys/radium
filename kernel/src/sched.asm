cef8ab33f66fb3a48475a2f7d4db461b29911c20
Charlie Somerville <charlie@charliesomerville.com>
2014-06-08 22:24:24 +1000

    use sysexit instead of iret to get to ring 3

--- kernel/src/sched.asm
+++ kernel/src/sched.asm
@@ -6,14 +6,12 @@ global sched_begin_multitasking
 %define USER_DATA (0x20 | 3)
 
 sched_begin_multitasking:
-    push dword USER_DATA  ; ss
-    push dword 0          ; esp
-    pushf                 ; eflags
-    push dword USER_CODE  ; cs
-    push dword 0x10000000 ; esp
     mov ax, USER_DATA
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
-    iret
+
+    xor ecx, ecx        ; let userland set up its own stack
+    mov edx, 0x10000000 ; task entry point
+    sysexit

d89c243a8e999680fdfb9f6b73941929fb6bdcca
Charlie Somerville <charlie@charliesomerville.com>
2014-06-30 11:48:18 -0400

    rejigger syscall vectors

--- api/radium.h
+++ api/radium.h
@@ -4,11 +4,14 @@
 #define ENOSYS 1 // no such syscall
 #define EFAULT 2 // bad address
 
+// debug syscalls:
 #define SYS_REGDUMP     0
-#define SYS_EXIT        1
-#define SYS_YIELD       2
-#define SYS_FORK        3
-#define SYS_WAIT        4
-#define SYS_CONSOLE_LOG 5
+#define SYS_CONSOLE_LOG 1
+
+// process management syscalls:
+#define SYS_EXIT        2
+#define SYS_YIELD       3
+#define SYS_FORK        4
+#define SYS_WAIT        5
 
 #endif
